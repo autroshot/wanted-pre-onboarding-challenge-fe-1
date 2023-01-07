192c7a5e081fb50c0b6ec266510012f41f9d4cf7
autroshot <autroshot@gmail.com>
2023-01-07 21:47:42 +0900

    구현: 인증 폼 페이지 일부

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -1,3 +1,13 @@
+import { Container } from '@chakra-ui/react';
+import Head from 'next/head';
+
 export default function AuthForm() {
-  return <>로그인 및 회원가입 페이지</>;
+  return (
+    <>
+      <Head>
+        <title>로그인 및 회원가입 | ToDo</title>
+      </Head>
+      <Container>로그인 및 회원가입 페이지</Container>
+    </>
+  );
 }
