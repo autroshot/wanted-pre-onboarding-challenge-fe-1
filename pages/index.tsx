df5bbb617701b70aa368212c5d95efa038553797
autroshot <autroshot@gmail.com>
2023-01-07 13:34:21 +0900

    구현: 홈페이지의 title 변경

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>Create Next App</title>
+        <title>홈 | ToDo</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
       홈페이지
