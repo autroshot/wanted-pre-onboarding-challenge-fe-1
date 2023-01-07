cc031a58c72a0a278ac5cc1a2080325f521ba9ec
autroshot <autroshot@gmail.com>
2023-01-08 01:17:03 +0900

    리팩터링: 로그인 페이지에서 AuthForm 컴포넌트 추출

---
+++ components/authForm.tsx
@@ -0,0 +1,37 @@
+import {
+  Button,
+  FormControl,
+  FormErrorMessage,
+  FormLabel,
+  Heading,
+  Input,
+  InputGroup,
+  InputRightElement,
+  VStack,
+} from '@chakra-ui/react';
+
+export default function AuthForm() {
+  return (
+    <VStack spacing="5">
+      <Heading>로그인</Heading>
+      <FormControl variant="floating" id="email" isRequired>
+        <Input placeholder=" " />
+        <FormLabel>이메일</FormLabel>
+        <FormErrorMessage>유효하지 않은 이메일입니다.</FormErrorMessage>
+      </FormControl>
+      <FormControl variant="floating" id="password" isRequired>
+        <InputGroup>
+          <Input pr="4.5rem" type="password" placeholder=" " />
+          <InputRightElement width="4.5rem">
+            <Button h="1.75rem" size="sm">
+              표시
+            </Button>
+          </InputRightElement>
+        </InputGroup>
+        <FormLabel>비밀번호</FormLabel>
+        <FormErrorMessage>유효하지 않은 비밀번호입니다.</FormErrorMessage>
+      </FormControl>
+      <Button w="100%">로그인</Button>
+    </VStack>
+  );
+}
--- pages/auth.tsx
+++ pages/auth.tsx
@@ -1,45 +1,15 @@
-import {
-  Button,
-  Container,
-  FormControl,
-  FormErrorMessage,
-  FormLabel,
-  Heading,
-  Input,
-  InputGroup,
-  InputRightElement,
-  VStack,
-} from '@chakra-ui/react';
+import { Container } from '@chakra-ui/react';
 import Head from 'next/head';
+import AuthForm from '../components/authForm';
 
-export default function AuthForm() {
+export default function Auth() {
   return (
     <>
       <Head>
         <title>로그인 및 회원가입 | ToDo</title>
       </Head>
       <Container>
-        <VStack spacing="5">
-          <Heading>로그인</Heading>
-          <FormControl variant="floating" id="email" isRequired>
-            <Input placeholder=" " />
-            <FormLabel>이메일</FormLabel>
-            <FormErrorMessage>유효하지 않은 이메일입니다.</FormErrorMessage>
-          </FormControl>
-          <FormControl variant="floating" id="password" isRequired>
-            <InputGroup>
-              <Input pr="4.5rem" type="password" placeholder=" " />
-              <InputRightElement width="4.5rem">
-                <Button h="1.75rem" size="sm">
-                  표시
-                </Button>
-              </InputRightElement>
-            </InputGroup>
-            <FormLabel>비밀번호</FormLabel>
-            <FormErrorMessage>유효하지 않은 비밀번호입니다.</FormErrorMessage>
-          </FormControl>
-          <Button w="100%">로그인</Button>
-        </VStack>
+        <AuthForm />
       </Container>
     </>
   );
