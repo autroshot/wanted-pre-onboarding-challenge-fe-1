a66d3e1c12d27a53ce40d8ffd5fadea2c7fcbea2
autroshot <autroshot@gmail.com>
2023-01-08 01:33:13 +0900

    구현: 비밀번호 표시 기능

--- components/forms/password.tsx
+++ components/forms/password.tsx
@@ -7,15 +7,18 @@ import {
   InputGroup,
   InputRightElement,
 } from '@chakra-ui/react';
+import { useState } from 'react';
 
 export default function PasswordForm() {
+  const [show, setShow] = useState(false);
+
   return (
     <FormControl variant="floating" id="password" isRequired>
       <InputGroup>
-        <Input pr="4.5rem" type="password" placeholder=" " />
+        <Input pr="4.5rem" type={show ? 'text' : 'password'} placeholder=" " />
         <InputRightElement width="4.5rem">
-          <Button h="1.75rem" size="sm">
-            표시
+          <Button h="1.75rem" size="sm" onClick={() => setShow(!show)}>
+            {show ? '숨기기' : '표시'}
           </Button>
         </InputRightElement>
       </InputGroup>
