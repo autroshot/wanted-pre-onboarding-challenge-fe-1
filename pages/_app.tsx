b4ca5a6e490108fc9d22abc163980be8271a4159
autroshot <autroshot@gmail.com>
2023-01-07 21:56:17 +0900

    구현: 차크라 UI 커스텀 테마 app에 적용

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,12 @@
 import { ChakraProvider } from '@chakra-ui/react';
 import type { AppProps } from 'next/app';
 import Layout from '../components/layout';
+import { theme } from '../styles/customChakra';
 import '../styles/globals.css';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
       <Layout>
         <Component {...pageProps} />
       </Layout>
