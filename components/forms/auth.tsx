b80fb383474f9c99738b860d5defc781c3f4ab93
autroshot <autroshot@gmail.com>
2023-01-08 02:16:36 +0900

    구현: 인증 폼의 제목 제거하고 CSS 변경

--- components/forms/auth.tsx
+++ components/forms/auth.tsx
@@ -1,9 +1,9 @@
 import {
+  Box,
   Button,
   FormControl,
   FormErrorMessage,
   FormLabel,
-  Heading,
   Input,
   VStack,
 } from '@chakra-ui/react';
@@ -11,15 +11,16 @@ import PasswordForm from './password';
 
 export default function AuthForm() {
   return (
-    <VStack spacing="5">
-      <Heading>로그인</Heading>
-      <FormControl variant="floating" id="email" isRequired>
-        <Input placeholder=" " />
-        <FormLabel>이메일</FormLabel>
-        <FormErrorMessage>유효하지 않은 이메일입니다.</FormErrorMessage>
-      </FormControl>
-      <PasswordForm />
-      <Button w="100%">로그인</Button>
-    </VStack>
+    <Box my="5">
+      <VStack spacing="5">
+        <FormControl variant="floating" id="email" isRequired>
+          <Input placeholder=" " />
+          <FormLabel>이메일</FormLabel>
+          <FormErrorMessage>유효하지 않은 이메일입니다.</FormErrorMessage>
+        </FormControl>
+        <PasswordForm />
+        <Button w="100%">로그인</Button>
+      </VStack>
+    </Box>
   );
 }
