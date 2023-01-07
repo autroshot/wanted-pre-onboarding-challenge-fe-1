5d1de83b70d66b9dcff70e6454ce00778fa08f3b
autroshot <autroshot@gmail.com>
2023-01-07 17:58:18 +0900

    구현: 네비바의 로그인 버튼을 a 태그로 감싸고 네비바 CSS 일부 구현

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,17 +1,37 @@
-import { Button, Flex, Link, Spacer } from '@chakra-ui/react';
+import {
+  Box,
+  Button,
+  Flex,
+  Link,
+  LinkBox,
+  LinkOverlay,
+  Spacer,
+} from '@chakra-ui/react';
 import NextLink from 'next/link';
 
 export default function Navbar() {
   return (
-    <Flex alignItems="center">
+    <Flex
+      px={6}
+      py={3}
+      gap={5}
+      alignItems="center"
+      direction={{ base: 'column', md: 'row' }}
+    >
       <Link as={NextLink} href="/">
         홈
       </Link>
       <Link as={NextLink} href="/">
         ToDo
       </Link>
-      <Spacer />
-      <Button>로그인</Button>
+      <Spacer display={{ base: 'none', md: 'block' }} />
+      <LinkBox>
+        <NextLink href="#">
+          <LinkOverlay>
+            <Button as={Box}>로그인</Button>
+          </LinkOverlay>
+        </NextLink>
+      </LinkBox>
     </Flex>
   );
 }
