fc02e2e729fbfd6c9164d00d67ce648a744e3844
autroshot <autroshot@gmail.com>
2023-01-08 13:09:00 +0900

    구현: 로그인 성공 시의 논리

--- components/forms/auth.tsx
+++ components/forms/auth.tsx
@@ -1,19 +1,23 @@
 import { Box, Button, VStack } from '@chakra-ui/react';
 import axios from 'axios';
+import { useRouter } from 'next/router';
 import { SubmitHandler, useForm } from 'react-hook-form';
 import EmailForm from './email';
 import PasswordForm from './password';
 
 export default function AuthForm({ APIURL, buttonText }: Props) {
   const { register, handleSubmit } = useForm<Inputs>();
+  const router = useRouter();
 
   const onSubmit: SubmitHandler<Inputs> = (data) => {
     console.log(data);
 
     axios
-      .post(APIURL, { ...data })
+      .post<UsersResponseData>(APIURL, { ...data })
       .then((res) => {
         console.log(res);
+        localStorage.setItem('loginToken', res.data.token);
+        router.push('/');
       })
       .catch((err) => {
         console.log(err);
@@ -37,6 +41,11 @@ export default function AuthForm({ APIURL, buttonText }: Props) {
   interface Inputs {
     email: string;
     password: string;
+  }
+
+  interface UsersResponseData {
+    message: string;
+    token: string;
   }
 }
 
