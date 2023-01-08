6d950a920888a460ab2ed91c32eb867c299d2559
autroshot <autroshot@gmail.com>
2023-01-08 11:58:26 +0900

    구현: 로그인 페이지에 폼 제출 함수 생성

--- components/forms/auth.tsx
+++ components/forms/auth.tsx
@@ -1,18 +1,26 @@
 import { Box, Button, VStack } from '@chakra-ui/react';
-import { useForm } from 'react-hook-form';
+import { SubmitHandler, useForm } from 'react-hook-form';
 import EmailForm from './email';
 import PasswordForm from './password';
 
 export default function AuthForm({ buttonText }: Props) {
   const { register, handleSubmit } = useForm<Inputs>();
 
+  const onSubmit: SubmitHandler<Inputs> = (data) => {
+    console.log(data);
+  };
+
   return (
     <Box my="5">
-      <VStack spacing="5">
-        <EmailForm name="email" register={register} />
-        <PasswordForm name="password" register={register} />
-        <Button w="100%">{buttonText}</Button>
-      </VStack>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <VStack spacing="5">
+          <EmailForm name="email" register={register} />
+          <PasswordForm name="password" register={register} />
+          <Button w="100%" type="submit">
+            {buttonText}
+          </Button>
+        </VStack>
+      </form>
     </Box>
   );
 
