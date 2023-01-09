3507aea0c91b8b86b8ad84d0aa470d02caf14cef
autroshot <autroshot@gmail.com>
2023-01-09 19:31:48 +0900

    구현: ToDo 페이지에 비로그인 사용자를 막는 논리와 로그인 페이지로 향하는 모달

--- pages/todos/[id].tsx
+++ pages/todos/[id].tsx
@@ -1,13 +1,52 @@
-import { Container } from '@chakra-ui/react';
+import {
+  Button,
+  Center,
+  Container,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  ModalOverlay,
+} from '@chakra-ui/react';
 import Head from 'next/head';
+import { useRouter } from 'next/router';
+import { useIsLogined } from '../../utils/auth';
 
 export default function ToDo() {
+  const router = useRouter();
   return (
     <>
       <Head>
         <title>ToDo | ToDo</title>
       </Head>
-      <Container my="5">ToDo 페이지</Container>
+      {!useIsLogined() ? (
+        <Modal
+          size="sm"
+          closeOnOverlayClick={false}
+          isOpen={true}
+          onClose={handleModalClose}
+        >
+          <ModalOverlay />
+          <ModalContent>
+            <ModalHeader>알림</ModalHeader>
+            <ModalCloseButton />
+            <ModalBody>
+              <Center>로그인이 필요합니다.</Center>
+            </ModalBody>
+            <ModalFooter>
+              <Button onClick={handleModalClose}>확인</Button>
+            </ModalFooter>
+          </ModalContent>
+        </Modal>
+      ) : (
+        <Container my="5">ToDo 페이지</Container>
+      )}
     </>
   );
+
+  function handleModalClose() {
+    router.push('/auth');
+  }
 }
