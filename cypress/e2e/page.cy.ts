c5a4ebba79edeeb64efd3b174a4078bc0596bd19
autroshot <autroshot@gmail.com>
2023-01-09 20:54:38 +0900

    테스트: 페이지 테스트 작성

--- cypress/e2e/page.cy.ts
+++ cypress/e2e/page.cy.ts
@@ -1,7 +1,11 @@
-describe('네비바로 페이지 방문', () => {
-  beforeEach(() => {
+describe('페이지', () => {
+  it('홈', () => {
     cy.visit('/');
   });
-
-  it('홈페이지', () => {});
+  it('ToDo', () => {
+    cy.visit('/todos/index');
+  });
+  it('로그인 및 회원가입', () => {
+    cy.visit('/auth');
+  });
 });
