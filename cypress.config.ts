0e38e3dbf2cb3bb842c2c762a8c60572834af858
autroshot <autroshot@gmail.com>
2023-01-09 21:36:58 +0900

    설정: 사이프러스에서 experimentalRunAllSpecs 설정

--- cypress.config.ts
+++ cypress.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from 'cypress';
 export default defineConfig({
   e2e: {
     baseUrl: 'http://localhost:3000',
+    experimentalRunAllSpecs: true,
   },
   watchForFileChanges: false,
 });
