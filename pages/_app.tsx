96e905dec0558c342ee1dc07b2ab793f4b72c2fe
autroshot <autroshot@gmail.com>
2023-01-09 19:03:05 +0900

    리팩터링: 미사용 CSS 파일 제거

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,6 @@ import { ChakraProvider } from '@chakra-ui/react';
 import type { AppProps } from 'next/app';
 import Layout from '../components/layout';
 import { theme } from '../styles/customChakra';
-import '../styles/globals.css';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
