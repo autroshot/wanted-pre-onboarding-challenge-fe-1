8c17542455c7bd7c1628d529b64c8b355f9020f0
autroshot <autroshot@gmail.com>
2023-01-09 11:16:00 +0900

    구현: 홈페이지에 콘텐츠 추가

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,34 @@
+import { Container, Heading, Text } from '@chakra-ui/react';
 import Head from 'next/head';
+import { useEffect, useState } from 'react';
+import MyStorage from '../utils/myStorage';
 
 export default function Home() {
+  const [myStorage, setMyStorage] = useState<null | MyStorage>(null);
+
+  useEffect(() => {
+    setMyStorage(new MyStorage(localStorage));
+  }, []);
+
   return (
     <>
       <Head>
         <title>홈 | ToDo</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      홈페이지
+      <Container my="5">
+        <Heading size="md">
+          {isLogined() ? '회원님 ' : null}ToDo에 오신 것을 환영합니다!
+        </Heading>
+        <Text mt="5">
+          이곳은 원티드 프리온보딩 챌린지 프론트엔드 코스 사전과제를 위해
+          만들어진 ToDo 프로젝트입니다.
+        </Text>
+      </Container>
     </>
   );
+
+  function isLogined() {
+    return myStorage && myStorage.getLoginToken() !== null;
+  }
 }
