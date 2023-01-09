abd6b76ad2a651a6d5c4b68b369b57e7aa9bef78
autroshot <autroshot@gmail.com>
2023-01-09 11:19:38 +0900

    구현: ToDo 페이지의 콘텐츠 일부

--- pages/todos/[id].tsx
+++ pages/todos/[id].tsx
@@ -1,3 +1,13 @@
+import { Container } from '@chakra-ui/react';
+import Head from 'next/head';
+
 export default function ToDo() {
-  return <>ToDo 페이지</>;
+  return (
+    <>
+      <Head>
+        <title>ToDo | ToDo</title>
+      </Head>
+      <Container my="5">ToDo 페이지</Container>
+    </>
+  );
 }
