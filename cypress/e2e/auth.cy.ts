ad4763f5211d85a59be1bdc5547c230cde55ea96
autroshot <autroshot@gmail.com>
2023-01-10 18:10:44 +0900

    테스트: '인증'의 '로그아웃' 작성

--- cypress/e2e/auth.cy.ts
+++ cypress/e2e/auth.cy.ts
@@ -1,14 +1,14 @@
-import { it } from 'node:test';
+import { login } from '../../utils/auth';
 
 describe('인증', () => {
+  const seededUser = { email: 'hong@gamil.com', password: '12345678' };
+
   beforeEach(() => {
     cy.request('GET', `${Cypress.env('server_url')}/seed`);
     cy.visit('/auth');
   });
 
   it('로그인', () => {
-    const seededUser = { email: 'hong@gamil.com', password: '12345678' };
-
     cy.get('[data-cy="loginForm"] [data-cy="emailInput"]').type(
       seededUser.email
     );
@@ -23,7 +23,25 @@ describe('인증', () => {
       expect(result[baseUrl]).to.have.property('loginToken');
     });
   });
-  it('로그아웃', () => {});
+
+  it('로그아웃', () => {
+    cy.request(
+      'POST',
+      `${Cypress.env('server_url')}/users/login`,
+      seededUser
+    ).then((res) => {
+      login(localStorage, res.body.token);
+      cy.visit('/');
+    });
+    cy.get('[data-cy="navbar"]').contains('로그아웃').click();
+
+    cy.get('[data-cy="navbar"]').contains('로그인');
+    cy.getAllLocalStorage().then((result) => {
+      const baseUrl = String(Cypress.config().baseUrl);
+      expect(result[baseUrl]).to.have.not.property('loginToken');
+    });
+  });
+
   it('회원가입 및 로그인', () => {
     const newUser = { email: 'dummy@gmail.com', password: 'abcd1234' };
   });
