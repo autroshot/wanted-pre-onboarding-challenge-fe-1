bc02fd55cd596831d374995198c4ce098254378b
autroshot <autroshot@gmail.com>
2023-01-17 00:17:58 +0900

    리팩터링: ToDo 페이지 - 상세 컨테이너에서 InputsComponent 추출

--- components/todo/detail/container.tsx
+++ components/todo/detail/container.tsx
@@ -3,10 +3,8 @@ import {
   Button,
   ButtonGroup,
   Flex,
-  Input,
   Spacer,
   Text,
-  Textarea,
   VStack,
 } from '@chakra-ui/react';
 import { MutableRefObject } from 'react';
@@ -18,6 +16,7 @@ import {
 import { Inputs, Todo } from '../../../pages/todos/[id]';
 import DeleteAlertDialog from '../deleteAlertDialog';
 import DefaultText from './defaultText';
+import InputsComponent from './inputs';
 
 export default function Container({
   todos,
@@ -62,22 +61,10 @@ export default function Container({
           onSubmit={handleSubmit}
         >
           <VStack spacing="2" h="100%">
-            <input type="hidden" {...register('id')} />
-            <Input
-              placeholder="제목"
-              readOnly={isEditMode ? false : true}
-              ref={(e) => {
-                ref(e);
-                titleRef.current = e;
-              }}
-              {...rest}
-            />
-            <Textarea
-              h="100%"
-              placeholder="내용"
-              resize="none"
-              readOnly={isEditMode ? false : true}
-              {...register('content')}
+            <InputsComponent
+              isEditMode={isEditMode}
+              titleRef={titleRef}
+              register={register}
             />
             <Box w="100%">
               <Text fontSize="xs">
---
+++ components/todo/detail/inputs.tsx
@@ -0,0 +1,40 @@
+import { Input, Textarea } from '@chakra-ui/react';
+import { MutableRefObject } from 'react';
+import { UseFormRegister } from 'react-hook-form';
+import { Inputs } from '../../../pages/todos/[id]';
+
+export default function InputsComponent({
+  isEditMode,
+  titleRef,
+  register,
+}: Props) {
+  const { ref, ...rest } = register('title');
+
+  return (
+    <>
+      <input type="hidden" {...register('id')} />
+      <Input
+        placeholder="제목"
+        readOnly={isEditMode ? false : true}
+        ref={(e) => {
+          ref(e);
+          titleRef.current = e;
+        }}
+        {...rest}
+      />
+      <Textarea
+        h="100%"
+        placeholder="내용"
+        resize="none"
+        readOnly={isEditMode ? false : true}
+        {...register('content')}
+      />
+    </>
+  );
+}
+
+export interface Props {
+  isEditMode: boolean;
+  titleRef: MutableRefObject<null | HTMLInputElement>;
+  register: UseFormRegister<Inputs>;
+}
