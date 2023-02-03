38173edbd035bd9ea4542f5b5e563e30e01df7e2
autroshot <autroshot@gmail.com>
2023-02-04 01:29:04 +0900

    리팩터링: text 상수의 순서 변경

--- constants/text.ts
+++ constants/text.ts
@@ -1,7 +1,7 @@
-export const NOTICE = '알림';
-export const LOGIN = '로그인';
+export const CANCEL = '취소';
+export const COMPLETE = '완료';
 export const CONFIRM = '확인';
 export const DELETE = '삭제';
 export const EDIT_MODE = '수정 모드';
-export const CANCEL = '취소';
-export const COMPLETE = '완료';
+export const LOGIN = '로그인';
+export const NOTICE = '알림';
