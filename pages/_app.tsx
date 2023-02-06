54a0356364419f4acc7f3dd6674275ac41b7f4c0
autroshot <autroshot@gmail.com>
2023-02-06 18:14:49 +0900

    구현: 앱에 탠스택 쿼리 제공자 추가

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,19 @@
 import { ChakraProvider } from '@chakra-ui/react';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import type { AppProps } from 'next/app';
 import Layout from '../components/layout';
 import { theme } from '../styles/customChakra';
 
 export default function App({ Component, pageProps }: AppProps) {
+  const queryClient = new QueryClient();
+
   return (
     <ChakraProvider theme={theme}>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
+      <QueryClientProvider client={queryClient}>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </QueryClientProvider>
     </ChakraProvider>
   );
 }
