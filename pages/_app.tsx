6d274b4ac8ceadf0131fbcb4daa63443088c60e2
autroshot <autroshot@gmail.com>
2023-02-12 01:45:30 +0900

    디버그: 앱 컴포넌트의 잘못된 쿼리 클라이언트

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,12 @@
 import { ChakraProvider } from '@chakra-ui/react';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import type { AppProps } from 'next/app';
+import { useState } from 'react';
 import Layout from '../components/layout';
 import { theme } from '../styles/customChakra';
 
 export default function App({ Component, pageProps }: AppProps) {
-  const queryClient = new QueryClient();
+  const [queryClient] = useState(() => new QueryClient());
 
   return (
     <ChakraProvider theme={theme}>
