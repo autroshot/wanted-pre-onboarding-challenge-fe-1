1b63db3764f685be6cc53cc95bbde5bc3d3921a2
autroshot <autroshot@gmail.com>
2023-02-15 00:19:01 +0900

    리팩터링: 로그인 fetcher 추출

---
+++ fetchers/auth.ts
@@ -0,0 +1,29 @@
+import axios, { AxiosResponse } from 'axios';
+
+export async function login(email: string, password: string) {
+  const axiosInstance = createAxiosInstance();
+
+  const res = await axiosInstance.post<
+    PostResponseData,
+    AxiosResponse<PostResponseData>,
+    PostRequestData
+  >('/users/login', { email, password });
+
+  return res.data;
+
+  interface PostRequestData {
+    email: string;
+    password: string;
+  }
+
+  interface PostResponseData {
+    message: string;
+    token: string;
+  }
+}
+
+function createAxiosInstance() {
+  return axios.create({
+    baseURL: process.env.NEXT_PUBLIC_SERVER_URL,
+  });
+}
