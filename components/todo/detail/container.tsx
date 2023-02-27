88f0573d01fa2c6d6c2a0c8cfbb4af3b3b185749
autroshot <autroshot@gmail.com>
2023-02-27 21:36:43 +0900

    리팩터링: ToDo 상세 컨테이너 - 조건문 논리 변경

--- components/todo/detail/container.tsx
+++ components/todo/detail/container.tsx
@@ -47,65 +47,64 @@ export default function Container({
     !isValidTodoId(selectedTodoId, todos)
   ) {
     return <DefaultText />;
-  } else {
-    const selectedTodo = todos.find(
-      (todo) => todo.id === selectedTodoId
-    ) as TodoType;
-    setValue('id', selectedTodo.id);
-    setValue('title', selectedTodo.title);
-    setValue('content', selectedTodo.content);
-
-    return (
-      <>
-        <Box
-          as="form"
-          p="3"
-          borderWidth="1px"
-          borderRadius="lg"
-          minH="16rem"
-          onSubmit={onSubmit}
-        >
-          <VStack spacing="2" h="100%">
-            <Inputs
-              isEditMode={isEditMode}
-              titleRef={titleRef}
-              register={register}
-            />
-
-            <VStack w="100%" spacing="2" align="start">
-              <DisplayTime
-                title="생성된 시간"
-                ISOString={selectedTodo.createdAt}
-                dataCy="createdAt"
-              />
-              <DisplayTime
-                title="수정된 시간"
-                ISOString={selectedTodo.updatedAt}
-                dataCy="updatedAt"
-              />
-            </VStack>
-
-            <Flex w="100%">
-              <Spacer />
-              <Buttons
-                isEditMode={isEditMode}
-                onAlertDialogOpen={onAlertDialogOpen}
-                onActivateEditModeClick={onActivateEditModeClick}
-                onDeactivateEditModeClick={onDeactivateEditModeClick}
-              />
-            </Flex>
-          </VStack>
-        </Box>
-
-        <DeleteAlertDialog
-          isOpen={isAlertDialogOpen}
-          todoTitle={selectedTodo.title}
-          onClose={onAlertDialogClose}
-          onDelete={() => onTodoDelete(selectedTodoId)}
-        />
-      </>
-    );
   }
+  const selectedTodo = todos.find(
+    (todo) => todo.id === selectedTodoId
+  ) as TodoType;
+  setValue('id', selectedTodo.id);
+  setValue('title', selectedTodo.title);
+  setValue('content', selectedTodo.content);
+
+  return (
+    <>
+      <Box
+        as="form"
+        p="3"
+        borderWidth="1px"
+        borderRadius="lg"
+        minH="16rem"
+        onSubmit={onSubmit}
+      >
+        <VStack spacing="2" h="100%">
+          <Inputs
+            isEditMode={isEditMode}
+            titleRef={titleRef}
+            register={register}
+          />
+
+          <VStack w="100%" spacing="2" align="start">
+            <DisplayTime
+              title="생성된 시간"
+              ISOString={selectedTodo.createdAt}
+              dataCy="createdAt"
+            />
+            <DisplayTime
+              title="수정된 시간"
+              ISOString={selectedTodo.updatedAt}
+              dataCy="updatedAt"
+            />
+          </VStack>
+
+          <Flex w="100%">
+            <Spacer />
+            <Buttons
+              isEditMode={isEditMode}
+              onAlertDialogOpen={onAlertDialogOpen}
+              onActivateEditModeClick={onActivateEditModeClick}
+              onDeactivateEditModeClick={onDeactivateEditModeClick}
+            />
+          </Flex>
+        </VStack>
+      </Box>
+
+      <DeleteAlertDialog
+        isOpen={isAlertDialogOpen}
+        todoTitle={selectedTodo.title}
+        onClose={onAlertDialogClose}
+        onDelete={() => onTodoDelete(selectedTodoId)}
+      />
+    </>
+  );
 
   function isValidTodoId(todoId: string, todos: TodoType[]) {
     if (todoId.length !== 21) return false;
