0eab68dcfe9695dd522db722acfa22c674705ad5
autroshot <autroshot@gmail.com>
2023-02-28 19:19:50 +0900

    리팩터링: '삭제'을 용어 상수로 대체

--- components/todo/detail/deleteAlertDialog.tsx
+++ components/todo/detail/deleteAlertDialog.tsx
@@ -27,13 +27,13 @@ export default function DeleteAlertDialog({
       <AlertDialogOverlay>
         <AlertDialogContent>
           <AlertDialogHeader fontSize="lg" fontWeight="bold">
-            ToDo 삭제
+            ToDo {DELETE}
           </AlertDialogHeader>
 
           <AlertDialogBody>
             {todoTitle.length === 0
-              ? '해당 ToDo를 정말 삭제하시겠습니까?'
-              : `${todoTitle}을(를) 정말 삭제하시겠습니까?`}
+              ? `해당 ToDo를 정말 ${DELETE}하시겠습니까?`
+              : `${todoTitle}을(를) 정말 ${DELETE}하시겠습니까?`}
           </AlertDialogBody>
 
           <AlertDialogFooter>
