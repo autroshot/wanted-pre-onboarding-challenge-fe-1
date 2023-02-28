819a2c97dfa4a466d233eea5ff03b47110e61310
autroshot <autroshot@gmail.com>
2023-02-28 23:40:13 +0900

    리팩터링: 스토리지 유틸 - 타입명 변경 myStorageKeys -> keys

--- utils/storage.ts
+++ utils/storage.ts
@@ -5,15 +5,15 @@ export class MyStorage {
     this.#storage = storage;
   }
 
-  get(key: myStorageKeys): null | string {
+  get(key: keys): null | string {
     return this.#storage.getItem(key);
   }
-  set(key: myStorageKeys, value: string): void {
+  set(key: keys, value: string): void {
     return this.#storage.setItem(key, value);
   }
-  remove(key: myStorageKeys): void {
+  remove(key: keys): void {
     return this.#storage.removeItem(key);
   }
 }
 
-type myStorageKeys = 'loginToken';
+type keys = 'loginToken';
