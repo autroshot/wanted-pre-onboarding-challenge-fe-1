3b76ae7c11c7dbbe12ce485365f694c237e22dad
autroshot <autroshot@gmail.com>
2023-03-08 00:57:50 +0900

    구현: JWT API

---
+++ pages/api/jwt.ts
@@ -0,0 +1,22 @@
+import { login } from 'controllers/auth';
+import { NextApiRequest, NextApiResponse } from 'next';
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  try {
+    switch (req.method) {
+      case 'POST':
+        login(req, res);
+        break;
+
+      default:
+        res.status(405).end();
+        break;
+    }
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: '서버 오류' });
+  }
+}
