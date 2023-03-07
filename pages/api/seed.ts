1fb8d7f784796b8307b3f9ad0b78d3f2db2f64d9
autroshot <autroshot@gmail.com>
2023-03-07 21:19:33 +0900

    구현: 시드 API

--- pages/api/seed.ts
+++ pages/api/seed.ts
@@ -1,9 +1,24 @@
+import { DummyTodos, DummyUsers } from 'cypress/e2e/dummy';
+import { deleteAllTodos, insertSeedTodos } from 'db/todo';
+import { deleteAllUsers, insertUsers } from 'db/user';
 import { NextApiRequest, NextApiResponse } from 'next';
 
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
   try {
     switch (req.method) {
       case 'GET':
+        const dummyUserInputs = new DummyUsers().getUserInputs();
+        const dummyTodos = new DummyTodos().getTodosWithGeneratedId();
+
+        await deleteAllUsers();
+        await insertUsers(dummyUserInputs);
+
+        await deleteAllTodos();
+        await insertSeedTodos(dummyTodos);
+
         res.status(200).end();
         break;
 
