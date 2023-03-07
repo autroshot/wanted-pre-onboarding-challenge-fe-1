a247a011c23ac7e6cce1239baaca0fb0b6aa9835
autroshot <autroshot@gmail.com>
2023-03-08 01:24:40 +0900

    구현: 회원가입 API

---
+++ pages/api/users.ts
@@ -0,0 +1,22 @@
+import { signUp } from 'controllers/auth';
+import { NextApiRequest, NextApiResponse } from 'next';
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  try {
+    switch (req.method) {
+      case 'POST':
+        signUp(req, res);
+        break;
+
+      default:
+        res.status(405).end();
+        break;
+    }
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: '서버 오류' });
+  }
+}
