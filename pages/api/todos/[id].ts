76f56f69f94eba369bd54684734f20fd529354fa
autroshot <autroshot@gmail.com>
2023-03-08 02:23:01 +0900

    구현: ToDo API

---
+++ pages/api/todos/[id].ts
@@ -0,0 +1,26 @@
+import { deleteTodo, updateTodo } from 'controllers/todo';
+import { NextApiRequest, NextApiResponse } from 'next';
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  try {
+    switch (req.method) {
+      case 'PUT':
+        updateTodo(req, res);
+        break;
+
+      case 'DELETE':
+        deleteTodo(req, res);
+        break;
+
+      default:
+        res.status(405).end();
+        break;
+    }
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: '서버 오류' });
+  }
+}
---
+++ pages/api/todos/index.ts
@@ -0,0 +1,26 @@
+import { createTodo, getTodos } from 'controllers/todo';
+import { NextApiRequest, NextApiResponse } from 'next';
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  try {
+    switch (req.method) {
+      case 'GET':
+        getTodos(req, res);
+        break;
+
+      case 'POST':
+        createTodo(req, res);
+        break;
+
+      default:
+        res.status(405).end();
+        break;
+    }
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: '서버 오류' });
+  }
+}
