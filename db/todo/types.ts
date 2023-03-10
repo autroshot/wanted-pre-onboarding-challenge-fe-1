60d3f35a2dd3e4f3af560e3ad1e45865bba0b766
autroshot <autroshot@gmail.com>
2023-03-10 15:31:19 +0900

    리팩터링: TodoRow 타입 구현

--- db/todo/types.ts
+++ db/todo/types.ts
@@ -1,7 +1,33 @@
+import {
+  GoogleSpreadsheetRow,
+  GoogleSpreadsheetWorksheet,
+} from 'google-spreadsheet';
 import { Todo } from '../../types/todo';
 
 export interface DBTodo {
   [index: string]: string;
+  id: string;
+  title: string;
+  content: string;
+  created_at: string;
+  updated_at: string;
+}
+
+export class TodoRow extends GoogleSpreadsheetRow implements DBTodo {
+  constructor(
+    parentSheet: GoogleSpreadsheetWorksheet,
+    rowNumber: number,
+    data: any
+  ) {
+    super(parentSheet, rowNumber, data);
+
+    this.id = data.id;
+    this.title = data.title;
+    this.content = data.content;
+    this.created_at = data.created_at;
+    this.updated_at = data.updated_at;
+  }
+
   id: string;
   title: string;
   content: string;
--- db/todo/utils.ts
+++ db/todo/utils.ts
@@ -1,8 +1,7 @@
-import { GoogleSpreadsheetRow } from 'google-spreadsheet';
 import { nanoid } from 'nanoid';
 import { Todo, TodoInput } from '../../types/todo';
 import { getSheet } from '../utiles';
-import { DBTodo } from './types';
+import { DBTodo, TodoRow } from './types';
 
 export async function createDBTodo(todoInput: TodoInput): Promise<DBTodo> {
   return {
@@ -22,21 +21,19 @@ export async function addTodos(DBTodos: DBTodo[]): Promise<void> {
 
   await todoSheet.addRows(DBTodos);
 }
-export async function getTodoRow(
-  id: Todo['id']
-): Promise<GoogleSpreadsheetRow | null> {
+export async function getTodoRow(id: Todo['id']): Promise<TodoRow | null> {
   const todoSheet = await getSheet('todo');
-  const todoRows = await todoSheet.getRows();
+  const todoRows = (await todoSheet.getRows()) as TodoRow[];
 
   return todoRows.find((todoRow) => todoRow.id === id) || null;
 }
-export async function getTodoRows(): Promise<GoogleSpreadsheetRow[]> {
+export async function getTodoRows(): Promise<TodoRow[]> {
   const todoSheet = await getSheet('todo');
-  const todoRows = await todoSheet.getRows();
+  const todoRows = (await todoSheet.getRows()) as TodoRow[];
 
   return todoRows;
 }
-export function getTodos(todoRows: GoogleSpreadsheetRow[]): Todo[] {
+export function getTodos(todoRows: TodoRow[]): Todo[] {
   return todoRows.map(toTodo);
 }
 function toDBTodo(todo: Todo): DBTodo {
@@ -51,7 +48,7 @@ function toDBTodo(todo: Todo): DBTodo {
 export function toDBTodos(todos: Todo[]): DBTodo[] {
   return todos.map(toDBTodo);
 }
-export function toTodo(todoRow: GoogleSpreadsheetRow | DBTodo): Todo {
+export function toTodo(todoRow: TodoRow | DBTodo): Todo {
   return {
     id: todoRow.id,
     title: todoRow.title,
