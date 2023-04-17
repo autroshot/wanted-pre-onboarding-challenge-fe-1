77c3dcb03f619e68e1d80f6d61c11bae62215fe4
autroshot <autroshot@gmail.com>
2023-04-17 21:19:02 +0900

    구현(API 유틸): newControllerSwitch 함수

--- utils/api.ts
+++ utils/api.ts
@@ -49,6 +49,64 @@ const controllerSwitch: ControllerSwitch = async (
   }
 };
 
+async function newControllerSwitch(controllerByMethod: ControllerByMethod) {
+  const { POSTController, GETController, PUTController, DELETEController } =
+    controllerByMethod;
+
+  return async function handler(req: NextApiRequest, res: NextApiResponse) {
+    try {
+      switch (req.method) {
+        case 'POST':
+          if (!POSTController) {
+            res.status(405).end();
+            break;
+          }
+          await POSTController(req, res);
+          break;
+
+        case 'GET':
+          if (!GETController) {
+            res.status(405).end();
+            break;
+          }
+          await GETController(req, res);
+          break;
+
+        case 'PUT':
+          if (!PUTController) {
+            res.status(405).end();
+            break;
+          }
+          await PUTController(req, res);
+          break;
+
+        case 'DELETE':
+          if (!DELETEController) {
+            res.status(405).end();
+            break;
+          }
+          await DELETEController(req, res);
+          break;
+
+        default:
+          res.status(405).end();
+          break;
+      }
+    } catch (err) {
+      if (isAxiosError(err) && err.response?.status === 429) {
+        res.status(429).json({
+          message: '너무 많은 요청이 발생했습니다. 1분 후에 다시 시도해주세요.',
+        });
+
+        return;
+      }
+
+      console.error(err);
+      res.status(500).json({ message: '서버에 오류가 발생했습니다.' });
+    }
+  };
+}
+
 function handleErrorDecorator(
   controllerSwitch: ControllerSwitch
 ): ControllerSwitch {
@@ -85,4 +143,4 @@ interface ControllerByMethod {
 
 const decoratedControllerSwitch = handleErrorDecorator(controllerSwitch);
 
-export { decoratedControllerSwitch as controllerSwitch };
+export { decoratedControllerSwitch as controllerSwitch, newControllerSwitch };
