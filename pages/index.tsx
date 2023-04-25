af885da1ada6a438d156378a459e879ccdb55b1c
autroshot <autroshot@gmail.com>
2023-04-26 02:36:51 +0900

    기능(홈페이지): 본문 텍스트 변경

--- pages/index.tsx
+++ pages/index.tsx
@@ -17,8 +17,7 @@ export default function Home() {
           {isLogined ? '회원님 ' : null}ToDo에 오신 것을 환영합니다!
         </Heading>
         <Text mt="5">
-          이곳은 원티드 프리온보딩 챌린지 프론트엔드 코스 사전과제를 위해
-          만들어진 {TODO} 프로젝트입니다.
+          이곳은 할 일과 간단한 인증을 구현한 {TODO} 프로젝트입니다.
         </Text>
       </Container>
     </>
